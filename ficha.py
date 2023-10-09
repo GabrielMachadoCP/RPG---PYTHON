f73935db1787a111a2a2a01b52e82d0f1ec5745b
GabrielMachadoCP <rm99880@fiap.com.br>
2023-10-09 08:51:39 -0300

    Adicionando os textos e funções

--- ficha.py
+++ ficha.py
@@ -2,3 +2,19 @@
 # Bianca Dancs     rm 551645
 # Gabriel Pimentel rm 99880
 # Hellen Assis     rm 98284
+
+import random
+
+def fichaValores() :
+        habilidade = random.randint(1,6) + 6
+        energia = random.randint(1,6) + random.randint(1,6) + 12
+        sorte = random.randint(1,6) + 6
+
+        ficha = {
+                "Habilidade" : habilidade,
+                "Energia" : energia,
+                "Sorte" : sorte,
+                "Provisões" : 10
+                }
+
+        return ficha
--- rpg.py
+++ rpg.py
@@ -3,4 +3,26 @@
 # Gabriel Pimentel rm 99880
 # Hellen Assis     rm 98284
 
-# Branch Gabriel
+#Imports
+import random
+
+import ficha
+import textos
+import inimigos
+
+#Mostrando a ficha
+print("=" * 50)
+print("SUA FICHA\n")
+# print(ficha.fichaValores())
+for i in ficha.fichaValores() :
+    print(i)
+print("=" * 50)
+
+#Começando a condição de looping
+# while energia != 0 or energia > 0 :
+
+#Condição inicial:
+#Quer abrir a caixa? Sim(270) e Não (66)
+
+#66
+#Quer ir para Oeste ou Leste? Leste(119) e Oeste (293)
