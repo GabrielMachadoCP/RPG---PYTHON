1d781d927faa23292a32a5f2463dd5087a2269dd
GabrielMachadoP <gabrielmapi12@gmail.com>
2023-10-09 19:59:03 -0300

    Fazendo as alterações do prog principal (bianca)

--- ficha.py
+++ ficha.py
@@ -1,7 +1,7 @@
-# Ana zerlin       rm 98065
-# Bianca Dancs     rm 551645
-# Gabriel Pimentel rm 99880
-# Hellen Assis     rm 98284
+# Ana Zerlin        RM 98065
+# Bianca Dancs      RM 551645
+# Gabriel Pimentel  RM 99880
+# Hellen Assis      RM 98284
 
 import random
 
@@ -9,12 +9,5 @@ def fichaValores() :
         habilidade = random.randint(1,6) + 6
         energia = random.randint(1,6) + random.randint(1,6) + 12
         sorte = random.randint(1,6) + 6
-
-        ficha = {
-                "Habilidade" : habilidade,
-                "Energia" : energia,
-                "Sorte" : sorte,
-                "Provisões" : 10
-                }
-
-        return ficha
+        
+        return habilidade, energia, sorte
--- rpg.py
+++ rpg.py
@@ -1,21 +1,194 @@
-# Ana zerlin       rm 98065
-# Bianca Dancs     rm 551645
-# Gabriel Pimentel rm 99880
-# Hellen Assis     rm 98284
+# Ana Zerlin        RM 98065
+# Bianca Dancs      RM 551645
+# Gabriel Pimentel  RM 99880
+# Hellen Assis      RM 98284
 
 #Imports
 import random
 
 import ficha
 import textos
-import inimigos
+
+#Inimigos
+inimigos = {
+    "mantecora" : {
+        "Nome" : "Mantecóra",
+        "Habilidade" : 11,
+        "Energia" : 11
+    },
+
+    "mosca_gigante" : {
+        "Nome" : "Mosca Gigante",
+        "Habilidade" : 7,
+        "Energia" : 7
+    },
+
+    "minotauro" : {
+        "Nome" : "Minotauro",
+        "Habilidade" : 9,
+        "Energia" : 9
+    },
+
+    "hobgoblin" : {
+        "Nome" : "Hobgoblin",
+        "Habilidade" : 6,
+        "Energia" : 5
+    },
+
+    "orca1" : {
+        "Nome" : "Primeira Orca",
+        "Habilidade" : 5,
+        "Energia" : 5
+    },
+
+    "orca2" : {
+        "Nome" : "Segunda Orca",
+        "Habilidade" : 6,
+        "Energia" : 4
+    },
+
+    "goblin1" : {
+    "Nome" : "Primeiro Goblin",
+    "Habilidade" : 5,
+    "Energia" : 4
+    },
+
+    "goblin2" : {
+        "Nome" : "Segundo Goblin",
+        "Habilidade" : 5,
+        "Energia" : 5
+    },
+
+    "hobgoblin1" : {
+    "Nome" : "Primeiro Hobgoblin",
+    "Habilidade" : 7,
+    "Energia" : 5
+    },
+
+    "hobgoblin2" : {
+        "Nome" : "Segundo Hobgoblin",
+        "Habilidade" : 6,
+        "Energia" : 5
+    },
+
+    "erva" : {
+        "Nome" : "Erva",
+        "Habilidade" : 9,
+        "Energia" : 9
+    },
+
+    "anao" : {
+        "Nome" : "Anao",
+        "Habilidade" : 8,
+        "Energia" : 6
+    },
+
+    "cao1" : {
+    "Nome" : "Primeiro Cão de Guarda",
+    "Habilidade" : 7,
+    "Energia" : 7
+    },
+
+    "cao2" : {
+        "Nome" : "Segundo Cão de Guarda",
+        "Habilidade" : 7,
+        "Energia" : 8
+    },
+
+    "guardiao1" : {
+    "Nome" : "Primeiro Guardião Voador",
+    "Habilidade" : 7,
+    "Energia" : 8
+    },
+
+    "guardiao2" : {
+        "Nome" : "Segundo Guardião Voador",
+        "Habilidade" : 8,
+        "Energia" : 8
+    },
+
+    "besta" : {
+        "Nome" : "Besta Sangrenta",
+        "Habilidade" : 12,
+        "Energia" : 10
+    },
+
+    "diabo_poco" : {
+        "Nome" : "Diabo do Poço",
+        "Habilidade" : 12,
+        "Energia" : 15
+    },
+
+    "imitador" : {
+        "Nome" : "Imitador",
+        "Habilidade" : 9,
+        "Energia" : 8
+    },
+
+    "verme_rocha" : {
+        "Nome" : "Verme da Rocha",
+        "Habilidade" : 7,
+        "Energia" : 11
+    },
+
+    "throm" : {
+        "Nome" : "Throm",
+        "Habilidade" : 10,
+        "Energia" : 12
+    },
+
+    "ninja" : {
+        "Nome" : "Ninja",
+        "Habilidade" : 11,
+        "Energia" : 9
+    },
+
+    "demonio_espelho" : {
+        "Nome" : "Demônio do Espelho",
+        "Habilidade" : 10,
+        "Energia" : 10
+    },
+
+    "guerreiro_esqueleto" : {
+        "Nome" : "Guerreiro-Esqueleto",
+        "Habilidade" : 8,
+        "Energia" : 6
+    },
+
+    "troll_caverna" : {
+        "Nome" : "Troll da Caverna",
+        "Habilidade" : 10,
+        "Energia" : 11
+    },
+
+    "homem_caverna" : {
+        "Nome" : "Homem da Caverna",
+        "Habilidade" : 7,
+        "Energia" : 7
+    }
+}
+
+#Apresentação
+print("Bem-vindo ao Calabouço da Morte")
 
 #Mostrando a ficha
 print("=" * 50)
 print("SUA FICHA\n")
-# print(ficha.fichaValores())
-for i in ficha.fichaValores() :
-    print(i)
+
+#Recuperando valores para a ficha com a função
+habilidade, energia, sorte = ficha.fichaValores()
+
+ficha = {
+        "Habilidade" : habilidade,
+        "Energia" : energia,
+        "Sorte" : sorte,
+        "Provisoes" : 10
+        }
+
+print(f"Habilidade: {ficha['Habilidade']}")
+print(f"Energia: {ficha['Energia']}")
+print(f"Sorte: {ficha['Sorte']}")
+print(f"Provisões: {ficha['Provisoes']}")
 print("=" * 50)
 
 #Começando a condição de looping
